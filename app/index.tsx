03563a5720bf5f2f72727ce4f2ac44b6f30836b1
jpllinas02 <jean.llinas@gmail.com>
2026-01-07 17:34:40 -0500

    Remove HashRouter wrapping from App component

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -12,8 +11,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
